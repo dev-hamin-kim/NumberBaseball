d50bd74b81630733b32f185da3a5cf2cd79614db
Hamin Kim <dev.hamin.kim@gmail.com>
2024-11-07 15:13:11 +0900

    [fix] logic for checking zero in the first place was fixed.

--- NumberBaseball/main.swift
+++ NumberBaseball/main.swift
@@ -12,27 +12,25 @@ var hadRightGuess = false
 
 print("그럼 놀이를 시작하지. 규칙은 알고 있을 것이라 믿네.")
 game.newGame()
+print("숫자를 입력하게나.")
 
 while !hadRightGuess {
     
-    print("숫자를 입력하게나.")
-    
     let input = Array(readLine()!)
-//    var (strike, ball) = (0, 0)
     
     if input == game.answer {
         hadRightGuess = true
         print("축하하네, 정답이군.")
         break
     }
-
-    if input.count == 3
+    
+    if input.first == "0" {
+        print("첫 번째 자리는 0이 들어가지 않는다네.")
+    } else if input.count == 3
         && input[0] != input[1]
         && input[0] != input[2]
         && input[1] != input[2] {
         print("\(game.umpireCall(for: input).strike) 스트라이크, \(game.umpireCall(for: input).ball) 볼.")
-    } else if input.first == "0" {
-        print("첫 번째 자리는 0이 들어가지 않는다네.")
     } else {
         print("3자리의 서로 중복되지 않는 숫자만 입력이 가능하네.")
     }
